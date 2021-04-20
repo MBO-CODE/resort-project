22f04b6dd11d6af95d964d29af94ccbd13fe4098
Bogdan Morar <bogdanmorar5@gmail.com>
2021-04-20 13:05:13 +0300

    fixed cannot read property 0

--- src/components/Room.js
+++ src/components/Room.js
@@ -2,12 +2,13 @@ import React from "react";
 import { Link } from "react-router-dom";
 import defaultImg from "../images/room-1.jpeg";
 
-export default function Room(room) {
+// FIXED ! Prob was room wasn't into {} -> we didn't read the data ok.
+export default function Room({ room }) {
   const { name, slug, images, price } = room;
   return (
     <article className="room">
       <div className="img-container">
-          {/* images[0]  -> cannot read poperty 0
+        {/* images[0]  -> cannot read poperty 0
           FIX THIS SHIT! */}
         <img src={images[0] || defaultImg} alt="single room" />
       </div>
